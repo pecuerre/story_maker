6d5244e879e068a90b2fd4d4409e862f65469eb1
Pedro Quintero <pecuerre@gmail.com>
2026-09-05 14:13:26 +0400

    improve menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def active_if(*controllers)
+    controllers = controllers.map { |c| c.to_s }
+    " active" if controllers.include?(controller.controller_name)
+  end
+
+  def visible?(*controllers)
+    controllers = controllers.map { |c| c.to_s }
+    controllers.include?(controller.controller_name)
+  end
 end
